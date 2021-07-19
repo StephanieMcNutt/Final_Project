f3859eb319d30029b20444832279d588a95c07fb
StephanieMcNutt <stephie4321@gmail.com>
2021-07-19 17:46:40 -0400

    commit 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Final_Project
+# regionalization_model
+Machine Learning to Regionalize Data and perform geospatial clustering
+
+Read a csv of historical loads booked from 2018 - May 2021, evaluating customer mix, carrier mix, and limiting volume distribution to create optimized regions in the US for satellite offices.  Using seaborn, numpy and pysal, perform geospatial clustering using the origin state of shipments to regionalize across 5 clusters to optimize volume and balance office load.  This solves for issues including underdeveloped markets having less load volume causing poor KPIs from satellite offices which have less market to serve.
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.2 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
